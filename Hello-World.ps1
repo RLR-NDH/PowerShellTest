a408dff3454d23433076bbd8a8b37ff3d6b1b6b1
RLR-NDH <rlriedinger@gmail.com>
2019-12-05 22:30:53 -0500

    Parameterized Multi-Line String

--- Hello-World.ps1
+++ Hello-World.ps1
@@ -1,9 +1,7 @@
 Write-Host "Computer: " ${env:Computer}
 
-foreach ($Job in ${env:JobList}) {
+foreach ($Job in ${env:JobList}  -split '\r\n?|\n\r?') {
 
    Write-Host "`$Job = [" $Job "]"
 
 }
-
-
